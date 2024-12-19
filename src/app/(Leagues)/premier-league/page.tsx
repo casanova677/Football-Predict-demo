dc86d033ba05cda75b77d5ba54d738d4b636ba5f
Ofordile Chukwuebuka <45992529+casanova677@users.noreply.github.com>
2024-12-19 05:15:51 -0800

    Update page.tsx

--- src/app/(Leagues)/premier-league/page.tsx
+++ src/app/(Leagues)/premier-league/page.tsx
@@ -1,56 +1,56 @@
-import { filterLeague, getFixturesById} from "@/api"
+import { filterLeague, getFixturesById } from "@/api";
 import LeagueMatches from "@/components/LeagueMatches";
-import LeagueTable from "@/components/LeagueTable"
+import LeagueTable from "@/components/LeagueTable";
 import Livescore from "@/components/Livescore";
 import Standings from "@/components/Table";
 import { FixtureType } from "@/types";
 
 const English = async () => {
-  const leagueId = 152 ;
+  const leagueId = 152;
   const getEnglishLeague = await getFixturesById(leagueId);
   const englishFixtures = getEnglishLeague.result;
 
-
-  if (!englishFixtures ) {
+  if (!englishFixtures) {
     return (
-      <div className=" w-full">
-        <div className="py-2 px-2 my-2">
-          
-          <Livescore leagueId={leagueId}/>
-
-          <p className="text-center justify-center py-2">
-            No Fixtures Today 
+      <div className="w-full space-y-6">
+        <div className="p-4 space-y-4 rounded-lg shadow-md dark:bg-gray-700">
+          <Livescore leagueId={leagueId} />
+          <p className="text-center text-sm font-medium text-gray-500">
+            No Fixtures Today
           </p>
         </div>
 
-        <div className="py-2 px-2 my-2">
-          <p className="text-center justify-center py-2">
+        <div className="p-4 space-y-4 rounded-lg shadow-md dark:bg-gray-700">
+          <p className="text-center text-lg font-bold text-gray-500">
             Premier League Standings
           </p>
           <Standings leagueId={leagueId} />
         </div>
       </div>
-      
     );
-  } else{
-
-      return (
-        <div className='w-full'>
-          
-          <Livescore leagueId={leagueId}/>
-          
-            <div key={englishFixtures.id}>
-              <LeagueMatches leagueId={leagueId} data={englishFixtures} />
-            </div>
-          
-
-          <div className="py-2 px-2 my-2">
-            <p className="text-center justify-center py-2">Premier League Standings</p>
-            <Standings leagueId={leagueId} />
-          </div>
+  } else {
+    return (
+      <div className="w-full space-y-6">
+        <div className="p-4 space-y-4 rounded-lg shadow-md dark:bg-gray-700">
+          <Livescore leagueId={leagueId} />
         </div>
-      )
-    }
-}
 
-export default English
+        <div
+          className="p-4 space-y-4 rounded-lg shadow-md dark:bg-gray-700"
+          key={englishFixtures.id}
+        >
+          <LeagueMatches leagueId={leagueId} data={englishFixtures} />
+        </div>
+
+        <div className="p-4 space-y-4 rounded-lg shadow-md dark:bg-gray-700">
+          <p className="text-center text-sm font-medium text-gray-500">
+            Premier League Standings
+          </p>
+          <Standings leagueId={leagueId} />
+        </div>
+      </div>
+    );
+  }
+};
+
+export default English;
